febfa071a5c8fdc7172c4b00a9ee01d5148d1f37
swarup1411 <swarupmondal53650@gmail.com>
2026-07-03 22:14:17 +0530

    Add x2many searchbar module and test module version 17.0

---
+++ sale_searchbar_test/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Sale Searchbar Test',
+    'version': '17.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Test x2many searchbar on Sale Order',
+    'author': 'Ranthox',
+    'depends': [
+        'sale',
+        'x2many_searchbar',
+    ],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
