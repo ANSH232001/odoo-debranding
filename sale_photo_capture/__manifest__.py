a1acd5345878f0edf2bcc32463a6d205418e5c4a
takkii4411-boop <takkii4411@gmail.com>
2026-07-04 10:17:34 +0530

    added modules in odoo 18.0

---
+++ sale_photo_capture/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Sale Order Photo Capture',
+    'version': '18.0.1.0.0',
+    'summary': 'Capture photograph on sale orders using photograph_capture_widget',
+    'category': 'Sales',
+    'author': 'Ranthox Pty Ltd',
+    'website': 'https://ranthox.odoo.com',
+    'depends': ['sale', 'photograph_capture_widget'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
