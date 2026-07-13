28ae54d89b0ccdb9ec2a7f1b25c228e98f6b2bed
ANSH232001 <106385407+ANSH232001@users.noreply.github.com>
2026-07-13 15:00:22 +0530

    Delete sale_order directory

--- sale_order/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    excel_file = fields.Binary(string="Excel File", attachment=True)
-    excel_file_filename = fields.Char(string="Excel Filename")
